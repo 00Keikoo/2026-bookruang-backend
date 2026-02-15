d96efacea84d45aa3776416e539350a7df2fdcec
andikazafka <andikazafka04@gmail.com>
2026-02-15 21:20:28 +0700

    feat: status management done

--- BookRuangApi/Program.cs
+++ BookRuangApi/Program.cs
@@ -8,21 +8,40 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite("Data Source=bookruang.db"));
 
+// CORS - PENTING! Harus sebelum Build()
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-// Configure pipeline
+// Auto create database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.EnsureCreated();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// USE CORS - HARUS SEBELUM UseAuthorization!
+app.UseCors("AllowAll");
 
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
